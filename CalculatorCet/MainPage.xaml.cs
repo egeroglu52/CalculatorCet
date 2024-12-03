ba8d415b134abdf085cf223ecfa97503fd76fe11
Ege Eroglu <eroglu_ege@hotmail.com>
2024-12-03 17:31:49 +0300

    odev gelistirmeleri tamamlandı

--- CalculatorCet/MainPage.xaml.cs
+++ CalculatorCet/MainPage.xaml.cs
@@ -1,26 +1,26 @@
-﻿namespace CalculatorCet
+﻿using System.Globalization;
+
+namespace CalculatorCet
 {
     public partial class MainPage : ContentPage
     {
-
-
-
         public MainPage()
         {
             InitializeComponent();
         }
+        
+        private double FirstNumber;
 
+        private bool isFirstNumberAfterOperator = true;
 
-        double FirstNumber;
-        bool isFirstNumberAfterOperator = true;
-        Operator PreviousOperator = Operator.None;
-
+        private Operator PreviousOperator = Operator.None;
+        
+        private readonly Stack<double> storedValue = new Stack<double>();
 
         private void SubtractButton_Clicked(object sender, EventArgs e)
         {
             DoCalculation();
             PreviousOperator = Operator.Subtract;
-
         }
 
         private void MultiplyButton_Clicked(object sender, EventArgs e)
@@ -33,67 +33,77 @@
         {
             DoCalculation();
             PreviousOperator = Operator.Divide;
-
         }
 
         private void AddButton_Clicked(object sender, EventArgs e)
         {
-
             DoCalculation();
             PreviousOperator = Operator.Add;
-
         }
 
         void DoCalculation()
         {
-
             switch (PreviousOperator)
             {
                 case Operator.None:
-                    FirstNumber = Double.Parse(Display.Text);
+                    FirstNumber = GetNumber();
                     break;
                 case Operator.Add:
-                    FirstNumber = FirstNumber + Double.Parse(Display.Text);
+                    FirstNumber = FirstNumber + GetNumber();
                     break;
                 case Operator.Subtract:
-                    FirstNumber = FirstNumber - Double.Parse(Display.Text);
+                    FirstNumber = FirstNumber - GetNumber();
 
                     break;
                 case Operator.Multiply:
-                    FirstNumber = FirstNumber * Double.Parse(Display.Text);
+                    FirstNumber = FirstNumber * GetNumber();
 
                     break;
                 case Operator.Divide:
-                    FirstNumber = FirstNumber / Double.Parse(Display.Text);
+                    FirstNumber = FirstNumber / GetNumber();
 
                     break;
-
             }
+
+            Display.Text = string.Empty;
+
             isFirstNumberAfterOperator = true;
-            Display.Text = FirstNumber.ToString();
+            ResultValue.Text = "Result is " + FirstNumber;        
+        }
+
+        private double GetNumber()
+        {
+            string text = Display.Text;
+
+
+            if (text.Contains("."))
+            {
+                double.TryParse(text, NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double result);
+
+                return result;
+            }
+
+            return Double.Parse(text);
         }
 
         private void Digit_Clicked(object sender, EventArgs e)
         {
             Button digitButton = sender as Button;
-            if(isFirstNumberAfterOperator)
+            if (isFirstNumberAfterOperator)
             {
                 Display.Text = digitButton.Text;
                 isFirstNumberAfterOperator = false;
-
             }
             else
             {
                 Display.Text += digitButton.Text;
             }
-           
         }
 
         private void EqualButton_Clicked(object sender, EventArgs e)
         {
             DoCalculation();
             PreviousOperator = Operator.None;
-
         }
 
         private void CEButton_Clicked(object sender, EventArgs e)
@@ -106,9 +116,32 @@
         {
             Display.Text = "0";
             FirstNumber = 0;
-            PreviousOperator= Operator.None;
+            PreviousOperator = Operator.None;
             isFirstNumberAfterOperator = true;
         }
-    }
 
+        private void MRButton_Clicked(object? sender, EventArgs e)
+        {
+            double value = storedValue.Pop();
+            Display.Text = value.ToString();
+            FirstNumber = value;
+        }
+
+        private void MSButton_Clicked(object sender, EventArgs e)
+        {
+            storedValue.Push(GetNumber());
+        }
+
+        private void DotButton_Clicked(object sender, EventArgs e)
+        {
+           Display.Text += ".";
+        }
+
+        private void MCButton_Clicked(object sender, EventArgs e)
+        {
+            storedValue.Clear();
+            Display.Text = string.Empty;
+            FirstNumber = 0f;
+        }
+    }
 }
